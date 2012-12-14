2c991ba3684074577e44a423d27bb8134fe4abe0
Mitchell Berger <mitchb@mit.edu>
2012-12-14 11:26:03 +0000

    Stop using krb4.

--- clone.sh
+++ clone.sh
@@ -5,6 +5,9 @@
 # $Id$
 #
 # $Log$
+# Revision 1.4  2012/12/14 11:26:03  mitchb
+# Stop using krb4.
+#
 # Revision 1.3  2011/12/23 19:42:24  mitchb
 # Uncommitted change from forever ago to use -quiet with 'vos listvldb'
 #
@@ -17,13 +20,12 @@ header=/tmp/clone.hdr.$$
 mailto=sipb-afsreq@mit.edu
 mailrepl=sipb-afsreq@mit.edu
 host=`/bin/hostname`
-KRBTKFILE=/tmp/tkt_cloning; export KRBTKFILE
 KRB5CCNAME=/tmp/krb5cc_cloning; export KRB5CCNAME
 
-/usr/athena/bin/kinit -k
+/usr/athena/bin/kinit -5 -k
 /bin/athena/aklog $cell
 
-/usr/athena/bin/zwrite -q -c sipb-auto -i backups -s reynelda -O auto -m "Beginning automated cloning."
+/usr/local/bin/zwrite -q -c sipb-auto -i backups -s reynelda -O auto -m "Beginning automated cloning."
 
 touch /afs/.$cell/service/DOING_CLONING
 
@@ -50,7 +52,7 @@ fi
 
 rm -f /afs/.$cell/service/DOING_CLONING
 
-/usr/athena/bin/zwrite -q -c sipb-auto -i backups -s reynelda -O auto -m "Done with automated cloning."
+/usr/local/bin/zwrite -q -c sipb-auto -i backups -s reynelda -O auto -m "Done with automated cloning."
 
 /usr/athena/bin/kdestroy >/dev/null 2>&1
 /bin/athena/unlog $cell
